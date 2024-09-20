ef8c3b4daf8b313aafa0f75ae0a9e6853a7aa416
Borna <146658039+BornaAlizadeh@users.noreply.github.com>
2024-09-20 15:18:07 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ const menu = document.querySelector('#menu');
 const hlink = document.querySelector('#hlink');
 const fasolid = document.querySelector('.fa-solid');
 
-hamburger.addEventListener("click" , () => {
-    menu.classList.toggle('hidden')
-    fasolid.classList.toggle('fa-xmark')
-    maincont2.classList.toggle('hidden')
-    maincont2.classList.toggle('flex')
-})
+
+// hamburger.addEventListener("click" , () => {
+//     menu.classList.toggle('hidden')
+//     fasolid.classList.toggle('fa-xmark')
+//     maincont2.classList.toggle('hidden')
+//     maincont2.classList.toggle('flex')
+//     talkw.classList.toggle('hidden')
+//     talkw.classList.toggle('flex')
+// })
 
 
+// hlink.forEach(link =>{
+//     link.addEventListener('click',() =>{
+//         menu.classList.toggle('hidden')
+//         fasolid.classList.toggle('fa-xmark')
+//     })
+// })
 
-hlink.forEach(link =>{
-    link.addEventListener('click',() =>{
-        menu.classList.toggle('hidden')
-        fasolid.classList.toggle('fa-xmark')
-    })
-})
+
+// dark mode
+function addClass() {
+    var element = document.getElementById("ht-dark");
+    element.classList.toggle("dark");
+}
